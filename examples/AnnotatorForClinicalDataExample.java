6d08ead12b483e0ae496bef5d59c0a8270709cef
Scott Carrier <scarrier@us.ibm.com>
2020-05-19 17:05:32 -0400

    Add analyzeWithFlow, deleteTenantData and legacyDeploy Java SDK examples

--- examples/AnnotatorForClinicalDataExample.java
+++ examples/AnnotatorForClinicalDataExample.java
@@ -11,24 +11,29 @@
  * specific language governing permissions and limitations under the License.
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+ import java.io.File;
+ import java.io.FileNotFoundException;
+ import java.util.ArrayList;
+ import java.util.Arrays;
+ import java.util.List;
+ import java.util.Map;
 
-import com.ibm.cloud.sdk.core.http.Response;
-import com.ibm.cloud.sdk.core.http.ServiceCall;
-import com.ibm.cloud.sdk.core.security.Authenticator;
-import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
-import com.ibm.watson.health.acd.v1.AnnotatorForClinicalData;
-import com.ibm.watson.health.acd.v1.model.AcdProfile;
-import com.ibm.watson.health.acd.v1.model.Annotator;
-import com.ibm.watson.health.acd.v1.model.Annotator.Name;
-import com.ibm.watson.health.acd.v1.model.ContainerGroup;
-import com.ibm.watson.health.acd.v1.model.Flow;
-import com.ibm.watson.health.acd.v1.model.GetProfilesOptions;
-import com.ibm.watson.health.acd.v1.model.SymptomDisease;
-import com.ibm.watson.health.acd.v1.util.FlowUtil;
+ import com.ibm.cloud.sdk.core.http.Response;
+ import com.ibm.cloud.sdk.core.http.ServiceCall;
+ import com.ibm.cloud.sdk.core.security.Authenticator;
+ import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
+ import com.ibm.watson.health.acd.v1.AnnotatorForClinicalData;
+ import com.ibm.watson.health.acd.v1.model.AcdProfile;
+ import com.ibm.watson.health.acd.v1.model.Annotator;
+ import com.ibm.watson.health.acd.v1.model.Annotator.Name;
+ import com.ibm.watson.health.acd.v1.model.Concept;
+ import com.ibm.watson.health.acd.v1.model.ContainerGroup;
+ import com.ibm.watson.health.acd.v1.model.DeleteUserSpecificArtifactsOptions;
+ import com.ibm.watson.health.acd.v1.model.DeployCartridgeOptions;
+ import com.ibm.watson.health.acd.v1.model.DeployCartridgeResponse;
+ import com.ibm.watson.health.acd.v1.model.Flow;
+ import com.ibm.watson.health.acd.v1.model.GetProfilesOptions;
+ import com.ibm.watson.health.acd.v1.util.FlowUtil;
 
 /**
  * Sample ACD Java SDK Calls
@@ -37,7 +42,7 @@ public class AnnotatorForClinicalDataExample {
 
 	public static String version = "2020-05-18";
 	public static String ACD_APIKEY="<YOUR_APIKEY_HERE>";
-	public static String ACD_URL="https://us-south.wh-acd.cloud.ibm.com/wh-acd/api";
+	public static String ACD_URL="https://us-east.wh-acd.cloud.ibm.com/wh-acd/api";
 	public static String name="AnnotatorForClinicalData";
 	public static String Constants_PROFILE_ID  = "p51c";
 	public static String Constants_FLOW_ID  = "flow51asdf";
@@ -80,27 +85,34 @@ public class AnnotatorForClinicalDataExample {
 
 		List<String> annotators = new ArrayList<String>(
 				Arrays.asList(
-						Name.SYMPTOM_DISEASE,
-						Name.MEDICATION,
-						Name.HYPOTHETICAL));
+						Name.CONCEPT_DETECTION,
+						Name.NEGATION));
 
 		Flow flow = new FlowUtil.Builder().annotators(annotators).build();
 
-		String text = "Patient has.";
+		String text = "Patient has lung cancer, but did not smoke. She may consider chemotherapy as part of a treatment plan.";
 
 		ContainerGroup response = acd.analyze(text, flow);
 
-		System.out.println(response);
+		List<Concept> concepts = response.getConcepts();
 
-		List<SymptomDisease> sd = response.getSymptomDisease();
-
-
-
+		for (Concept c: concepts)
+			System.out.println("Type: " + c.getType() + " Name: " + c.getPreferredName());
 
 	}
 
 	// POST /analyze/{flowId}
 	public static void analyzeWithFlow() {
+
+		String flowId = "wh_acd.ibm_clinical_insights_v1.0_standard_flow";
+		String text = "Patient has lung cancer, but did not smoke. She may consider chemotherapy as part of a treatment plan.";
+
+		ContainerGroup response = acd.analyzeWithFlow(flowId, text);
+
+		List<Concept> concepts = response.getConcepts();
+
+		for (Concept c: concepts)
+			System.out.println("Type: " + c.getType() + " Name: " + c.getPreferredName());
 
 	}
 
@@ -171,7 +183,11 @@ public class AnnotatorForClinicalDataExample {
 	}
 
 	// DELETE /user_data
-	public static void deleteTenantConfigs() {
+	public static void deleteTenantData() {
+
+		DeleteUserSpecificArtifactsOptions opts = new DeleteUserSpecificArtifactsOptions();
+		Response<Void> resp = acd.deleteUserSpecificArtifacts(opts).execute();
+		System.out.println(resp.getStatusCode());
 
 	}
 
@@ -202,6 +218,19 @@ public class AnnotatorForClinicalDataExample {
 
 	// POST /deploy
 	public static void legacyDeploy() {
+
+		File cartridgeZip = new File("/tmp/my_cartridge_v1.0.zip");
+		DeployCartridgeOptions opts;
+		try {
+			opts = new DeployCartridgeOptions.Builder()
+					.archiveFile(cartridgeZip)
+					.archiveFileContentType("application/octet-stream")
+					.update(true).build();
+			Response<DeployCartridgeResponse> r = acd.deployCartridge(opts).execute();
+			System.out.println(r.getStatusCode());
+		} catch (FileNotFoundException e) {
+			// handle exception
+		}
 
 	}
 
